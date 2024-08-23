3817f180d9a60dd4fe5e5cd74ba1f4ade63ba0c8
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-08-22 23:46:23 -0700

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -145,6 +145,32 @@ for i in strs[0]:
        else:
            return True
 #####################################################################
+#Q33. Search in rotated sorted array
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l,r=0,len(nums)-1
+
+        while l<=r:
+            mid=(l+r)//2
+            if target == nums[mid]:
+                return mid
+
+   
+             #left portion
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l=mid+1
+                else:
+                    r=mid-1
+             #right postion
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r=mid-1
+                else:
+                    l=mid+1
+        return -1
+	    #o(log n) as no for loop is used
+#####################################################################
 #Q49.Group Anagrams
 #https://leetcode.com/problems/group-anagrams/
     from collections import defaultdict
