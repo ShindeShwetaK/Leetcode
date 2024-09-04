8676df15f8f3bbc90a87a2f01b407cf4243696b5
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-09-03 17:01:28 -0700

    Update 2pointersProblems.py

--- 2pointersProblems.py
+++ 2pointersProblems.py
@@ -22,3 +22,30 @@ for i , a in enumerate(nums):
 print(new)
 #sort=o(nlogn)+o(n^2) this is complete o(n^2) as it has 2 loops
 #######################################################################################
+#Q18.4Sum
+#https://leetcode.com/problems/4sum/
+        new=[]
+        nums.sort()
+        for i in range(len(nums)-3):
+            if i >0 and nums[i]==nums[i-1]:
+                continue       
+            for j in range(i+1,len(nums)-2):
+                if j >i+1 and nums[j]==nums[j-1]:
+                   continue
+                
+                l,r=j+1, len(nums)-1
+                while l<r:
+                   foursum= nums[i]+nums[j]+nums[l]+nums[r]
+                   if foursum>target:
+                       r-=1
+                   elif foursum<target:
+                       l+=1
+                   else:
+                       new.append([nums[i],nums[j],nums[l],nums[r]])
+                       while l<r and nums[l]==nums[l+1]:
+                          l+=1
+                       while l<r and nums[r]==nums[r-1]:
+                          r-=1
+                       l+=1
+                       r-=1
+        return new
