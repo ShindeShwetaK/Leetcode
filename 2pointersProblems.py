3ad95c3f6d95048f41f146b91981b256fdaabe01
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-09-04 07:56:59 -0700

    Update 2pointersProblems.py

--- 2pointersProblems.py
+++ 2pointersProblems.py
@@ -81,10 +81,19 @@ print(new)
                        r-=1
         return new
 #######################################################################################
-
-
-
-
+#Q.167.Two sum II with sorted array
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l,r=0,len(numbers)-1
+        while l < r :
+            cursum=numbers[l]+numbers[r]
+            if cursum > target :
+                r-=1
+            elif cursum < target :
+                l+=1
+            else:
+                return [l+1,r+1]
+        return []
 #######################################################################################
 #Q283. Move zeros
 #https://leetcode.com/problems/move-zeroes/description/
