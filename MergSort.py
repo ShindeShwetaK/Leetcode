0cb4f58e29abeef20c5f9d7d93e8f8603df3eed5
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-09-09 08:57:00 -0700

    Update MergSort.py

--- MergSort.py
+++ MergSort.py
@@ -21,5 +21,22 @@ class Solution:
             nums1[last]=nums2[n-1]
             n-=1
             last-=1
+##################################################
+#Q1209.remove-all-adjacent-duplicates-in-string-ii 
+s = "deeedbbcccbdaa"
+k = 3    
+stack=[]
+for c in s:
+    if stack and stack[-1][0]==c:
+        stack[-1][1]+=1
+    else:
+        stack.append([c,1])
+    if stack[-1][1]==k:
+        stack.pop()            
+res=""
+for char,count in stack:
+    res+=(char*count)
+print(res)
+#################################################
 
         
