f8cb7c7529b4ce65b8a6ea6b98158557d2c9f4a1
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-09-09 09:30:13 -0700

    Update 2pointersProblems.py

--- 2pointersProblems.py
+++ 2pointersProblems.py
@@ -45,6 +45,20 @@ def twoSum(nums,target):
                 r-=1
         return res
 ###########################################################################
+#Q5
+        if len(s) <= 1:
+           return s
+        
+        Max_Len=1
+        Max_Str=s[0]
+        for i in range(len(s)-1):
+            for j in range(i+1,len(s)):
+                if j-i+1 > Max_Len and s[i:j+1] == s[i:j+1][::-1]:
+                    Max_Len = j-i+1
+                    Max_Str = s[i:j+1]
+
+        return Max_Str
+#####################################################################
 #Q15.3Sum
 #https://leetcode.com/problems/3sum/description/
 nums = [-1,0,1,2,-1,-4]
