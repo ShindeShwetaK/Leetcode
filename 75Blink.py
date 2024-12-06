fa2b5fa7f0e20f986004f4cfc45ba94dba8b7ffc
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-12-06 09:53:19 -0800

    Update 75Blink.py

--- 75Blink.py
+++ 75Blink.py
@@ -151,7 +151,34 @@ class Solution:
 
 
         return False
-##########################################################################################################
+#######################################################################################
+##Q143###################
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            next_node = second.next
+            second.next = prev
+            prev = second
+            second = next_node
+
+        first , second = head , prev
+        while second:
+           temp_list1 , temp_list2 = first.next, second.next
+           first.next = second
+           second.next = temp_list1
+           first , second =  temp_list1 , temp_list2                                                                                                                                                                                
 
 
 
