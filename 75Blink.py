30d9326b0ee009d23e2507a6c1ebe9853e14190c
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-12-07 19:25:26 -0800

    Update 75Blink.py

--- 75Blink.py
+++ 75Blink.py
@@ -239,6 +239,50 @@ class Solution:
         return maxVal
 
 ####################################################################################
+# Adding 2 LL with carry
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        dummy = ListNode()
+        res = dummy
+
+        total = carry = 0
+
+        while l1 or l2 or carry:
+            total = carry
+
+            if l1:
+                total += l1.val
+                l1 = l1.next
+            if l2:
+                total += l2.val
+                l2 = l2.next
+            
+            num = total % 10
+            carry = total // 10
+            dummy.next = ListNode(num)
+            dummy = dummy.next
+        
+        return res.next
+#########################################################################################
+# Q19 Remove nth node from behind
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        res = ListNode(0,head)
+        dummy = res
+
+        for i in range(n):
+            head = head.next
+
+        while head:
+            head = head.next
+            dummy = dummy.next
+
+        dummy.next = dummy.next.next
+
+        return res.next
+    #########################################################################
+
 
 
 
