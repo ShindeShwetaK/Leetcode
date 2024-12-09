c1029b96e05c3fad52ffc3eb521cb1506f6246cd
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-12-08 18:26:45 -0800

    Update 75Blink.py

--- 75Blink.py
+++ 75Blink.py
@@ -652,6 +652,29 @@ class Solution:
         return -1
 
 ###################################################################
+#Q Implementing queue with stack
+class MyQueue:
+
+    def __init__(self):
+        self.q=deque()
+        
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        
+
+    def pop(self) -> int:
+        return self.q.popleft()
+        
+
+    def peek(self) -> int:
+        return self.q[0]
+        
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+######################################################################
+
 
 
 
