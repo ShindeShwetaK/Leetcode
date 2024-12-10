22cb880216eb9a77f2ffc94b9634f1b4e3400e9a
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-12-10 11:29:18 -0800

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -40,3 +40,27 @@ class Solution:
             return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
 
         return False
+
+#####################################################
+#Q101. Symmetric Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def mirror(n1,n2):
+            if not n1 and not n2:
+                return True
+
+            if not n1 or not n2:
+                return False
+
+            if n1.val == n2.val:
+                return mirror(n1.left, n2.right) and mirror(n1.right, n2.left)
+
+            return False
+            
+        return mirror(root.left , root.right)
