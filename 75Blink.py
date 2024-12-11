3459ec7c4cbf8ec1046a3ab445283f9b32ea8018
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2024-12-11 00:28:12 -0800

    Update 75Blink.py

--- 75Blink.py
+++ 75Blink.py
@@ -212,6 +212,34 @@ class Solution:
             current = next_node
 
         return prev
+#############################################################################################
+##Q 21 Merging 2 linkedlist
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+
+        while list1 and list2:
+            if list1.val > list2.val:
+                cur.next = list2
+                list2 = list2.next
+            else:
+                cur.next = list1
+                list1 = list1.next
+            
+            cur = cur.next
+        
+        if list1:
+            cur.next = list1
+        else:
+            cur.next = list2
+        
+        return dummy.next
 
 ############################################################################################
 ##Q2130 Max Sum for LL
