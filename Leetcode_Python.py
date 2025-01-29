83f88c680d658cb792325e25532014dd96b1fb0e
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2025-01-29 15:07:33 -0800

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -796,6 +796,16 @@ class Solution:
                 left = mid + 1
 
         return left
+############################################################
+#Q Remove duplicate II
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 2
+        for i in range(2, len(nums)):
+            if nums[j-2] != nums[i]:
+                nums[j] = nums[i]
+                j += 1
+        return j
 
 
 
