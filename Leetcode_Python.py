de8c10735428ec23755f16f3fb1d2c3b257f2fa8
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2025-02-04 11:32:24 -0800

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -856,6 +856,30 @@ class Solution:
                 return n - i
 
         return 0
+################################################################
+#q58. Length of Last Word
+ref = s.split()
+return len(ref[-1])
+########################################
+#Q134 Gas Station
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        start = 0
+        total_gas = 0
+        current_gas = 0
+
+        for i in range(len(gas)):
+            fuel_gain = gas[i] - cost[i]
+            total_gas += fuel_gain
+            current_gas += fuel_gain
+
+            if current_gas < 0:
+                current_gas = 0
+                start = i + 1
+
+        return -1 if total_gas < 0 else start
+
+################################################################
 
 
 
