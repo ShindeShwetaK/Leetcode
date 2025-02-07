ee00bb04e1d839da5dbe16ecafcd4912489cb747
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2025-02-07 11:59:44 -0800

    Update Leetcode_Python.py

--- Leetcode_Python.py
+++ Leetcode_Python.py
@@ -880,6 +880,24 @@ class Solution:
         return -1 if total_gas < 0 else start
 
 ################################################################
+#Q125 Valid Palindrom
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(c.lower() for c in s if c.isalnum())
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+##########################################################
+
 
 
 
