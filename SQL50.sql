4310b00c9b024b6c5b95bec1de840af868e008b7
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2026-01-13 15:44:21 -0800

    Update SQL50.sql

--- SQL50.sql
+++ SQL50.sql
@@ -597,6 +597,20 @@ FROM first_login f
 JOIN Activity a
   ON a.player_id = f.player_id
  AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY);
+
+WITH first_login AS (
+  SELECT player_id, MIN(event_date) AS first_date
+  FROM Activity
+  GROUP BY player_id
+)
+SELECT ROUND(
+  COUNT(DISTINCT a.player_id) / (SELECT COUNT(*) FROM first_login),
+  2
+) AS fraction
+FROM first_login f
+JOIN Activity a
+  ON a.player_id = f.player_id
+ AND DATEDIFF(a.event_date, f.first_date) = 1;
 ################################################
 
 
