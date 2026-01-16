234c354d01c38f6532f4611b9e5219de0faf0c33
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2026-01-15 18:07:47 -0800

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -194,4 +194,24 @@ class Solution:
             stack.append(i)
 
         return sum(res) % (10**9+7)
+
+########################################################################
+1047 Remove All Adjacent Duplicates In String
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+
+        for char in s:
+            if stack and stack[-1] == char:
+                stack.pop()
+            else:
+                stack.append(char)
+
+        return "".join(stack)
+
+###############################################################
+
+                
+    
+
   
