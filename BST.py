442ad564553d45e6b67e518e04b735bdbe3572cf
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2026-02-04 11:49:19 -0800

    Update BST.py

--- BST.py
+++ BST.py
@@ -61,3 +61,36 @@ class Solution:
             self.leafNode(root.left,leafNodeList)
             self.leafNode(root.right,leafNodeList)
 
+###########################################
+102. Binary Tree Level Order Traversal
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+
+        if not root:
+            return res
+
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            same_level = []       
+
+            for _ in range(len(q)):
+                node = q.popleft()
+                same_level.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            res.append(same_level)
+
+        return res
+
+###########################################
+
+
+
+
