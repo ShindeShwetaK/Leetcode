bf9237b6f39aa9b1aee929c5c942c65aafd29e28
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2026-02-05 20:23:42 -0800

    Update BST.py

--- BST.py
+++ BST.py
@@ -145,40 +145,4 @@ class Solution:
         return node_x[0] != node_y[0] and node_x[1] == node_y[1]
 
 ##################################################
-994. rotten oranges
 
-class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        visited = grid
-        q = collections.deque()
-        countFreshOrange = 0
-        for i in range(m):
-            for j in range(n):
-                if visited[i][j] == 2:
-                    q.append((i, j))
-                if visited[i][j] == 1:
-                    countFreshOrange += 1
-        if countFreshOrange == 0:
-            return 0
-        if not q:
-            return -1
-        
-        minutes = -1
-        dirs = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-        while q:
-            size = len(q)
-            while size > 0:
-                x, y = q.popleft()
-                size -= 1
-                for dx, dy in dirs:
-                    i, j = x + dx, y + dy
-                    if 0 <= i < m and 0 <= j < n and visited[i][j] == 1:
-                        visited[i][j] = 2
-                        countFreshOrange -= 1
-                        q.append((i, j))
-            minutes += 1
-        
-        if countFreshOrange == 0:
-            return minutes
-        return -1
