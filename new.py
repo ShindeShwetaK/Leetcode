76446831940c49862827982976d583330375d5f2
ShwetaShindek <160676251+ShindeShwetaK@users.noreply.github.com>
2026-03-08 18:42:26 -0700

    Update new.py

--- new.py
+++ new.py
@@ -228,7 +228,17 @@ class Solution:
         return longest
 
 ______________________________________________
+160. Intersection of Two Linked Lists
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        lista = headA
+        listb = headB
 
+        while lista != listb:
+            lista = lista.next if lista else headB
+            listb = listb.next if listb else headA
+
+        return listb
         
 
    
